4fd4b475c248274d3b6a16bfea063183a7ca6919
Jeff Khirvy Sarmen <khirv_19@yahoo.com>
2019-09-23 23:56:13 +0800

    feature: detect necessary fields onload

--- public/js/loan_create.js
+++ public/js/loan_create.js
@@ -1,5 +1,33 @@
-document.getElementById("amount").addEventListener("keydown", update_loan_detail);
-
 function update_loan_detail(){
-	alert('Changes Detected');
+	var loan_details = document.getElementById("loan-details");
+	
+	var amount_field = document.getElementById("amount");
+	var term_field = document.getElementById("term");
+	var interest_per_annum_field = document.getElementById("interest_per_annum");
+	var start_of_payment_field = document.getElementById("start_of_payment");
+	if( !(amount_field && term_field && interest_per_annum_field && start_of_payment_field) ){
+		loan_details.innerHTML = "Cannot find all fields necessary..";
+		return;
+	}
+	loan_details.innerHTML = "Changes detected";
 }
+
+
+$(document).ready(function() {
+	var loan_details = document.getElementById("loan-details");
+	
+	var amount_field = document.getElementById("amount");
+	var term_field = document.getElementById("term");
+	var interest_per_annum_field = document.getElementById("interest_per_annum");
+	var start_of_payment_field = document.getElementById("start_of_payment");
+	
+	amount_field.addEventListener("keydown", update_loan_detail );
+	term_field.addEventListener("keydown", update_loan_detail );
+	interest_per_annum_field.addEventListener("keydown", update_loan_detail );
+	start_of_payment_field.addEventListener("change", update_loan_detail );
+	
+	if( !(amount_field && term_field && interest_per_annum_field && start_of_payment_field) ){
+		loan_details.innerHTML = "Cannot find all fields necessary..";
+		return;
+	}
+});
