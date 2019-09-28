22fcd952dc1b66e8aa953f8cc498bf03b94179ee
Jeff Khirvy Sarmen <khirv_19@yahoo.com>
2019-09-28 21:12:47 +0800

    feature(loan_show): include payroll and payment schedule in loan show view

--- resources/views/loans/show.blade.php
+++ resources/views/loans/show.blade.php
@@ -3,28 +3,112 @@
 
 @section('content')
 	@include('partials.flash')
-
 	<div class="container bg-dark mt-3">
-		@accessright('loan_edit')
-			<a href="{{--{{ route('loans.edit', [$loan]) }}--}}" class="btn btn-warning">Edit</a>
-		@endaccessright
-		
-		@accessright('loan_delete')
-			<form action="{{ route('loans.destroy', [$loan]) }}" method="POST">
-				@method('DELETE')
-				@csrf
+		<br>
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="card">
+					<div class="card-header text-md-center">Loan Details</div>
+					<div id="loan-details" class="card-body">
+						<div class="row">
+							<div class="col-sm-3">Loan ID: </div>
+							<div class="col-sm-3">{{ $loan->id }}</div>
+							
+							<div class="col-sm-3">Member: </div>
+							<div class="col-sm-3"><a href="{{ route('members.show', [$loan->member]) }}" >{{ $loan->member->full_name }}</a></div>
+							
+							<div class="col-sm-3">Loan Type: </div>
+							<div class="col-sm-3">{{ $loan->loan_type_object->name }}</div>
+							
+							<div class="col-sm-3">Amount: </div>
+							<div class="col-sm-3">{{ $loan->amount }}</div>
+							
+							<div class="col-sm-3">Term: </div>
+							<div class="col-sm-3">{{ $loan->term }}</div>
+							
+							<div class="col-sm-3">Interest per annum: </div>
+							<div class="col-sm-3">{{ $loan->interest_per_annum }}</div>
+							
+							<div class="col-sm-3">Start of Payment: </div>
+							<div class="col-sm-3">{{ $loan->start_of_payment }}</div>
+						</div>
+						<hr>
+						<div class="row">
+							<div class="col-sm-1">Remarks: </div>
+							<div class="col-sm-7">{{ $loan->remarks }}</div>
+							
+							<div class="col-sm-1">Payroll: </div>
+							<div class="col-sm-3">
+								<ul>
+								@foreach($loan->payrolls()->get() as $payroll)
+									<li>{{ $payroll->name }}</li>
+								@endforeach
+								</ul>
+							</div>
+						</div>
+						<div class="row">
+							@accessright('loans_edit')
+								<div class="col-sm-1">
+									<a href="{{--{{ route('loans.edit', [$loan]) }}--}}" class="btn btn-warning">Edit</a>
+								</div>
+							@endaccessright
+							
+							@accessright('loans_delete')
+								<div class="col-sm-1">
+									<form action="{{ route('loans.destroy', [$loan]) }}" method="POST">
+										@method('DELETE')
+										@csrf
 
-				<button type="submit" class="btn btn-danger">Delete</button>
-			</form>
-		@endaccessright
+										<button type="submit" class="btn btn-danger">Delete</button>
+									</form>
+								</div>
+							@endaccessright
+						</div>
 
-		@foreach ($loan->getAttributes() as $column => $value)
-			<div class="row">
-				@if($column != 'profile_picture')
-					<label>{{ $loan->getColumnNameForView($column) }}</label>
-					<label>{{ $value }}</label>
-				@endif
+					</div>
+				</div>
 			</div>
-		@endforeach
+		</div>
+		<hr>
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="card">
+					<div class="card-header text-md-center">Payment Schedule</div>
+					<div id="loan-details" class="card-body">
+					
+						@foreach($loan->payrolls()->get() as $payroll)
+							<div class="row">
+								<h4>{{ $payroll->name }}</h4>
+								<div class="table">
+									<div class="row">
+										<div class="col-sm-1">Payment No.</div>
+										<div class="col-sm">Expected Payment Date</div>
+										<div class="col-sm">Actual Payment Date</div>
+										<div class="col-sm">Amount Paid</div>
+										<div class="col-sm">Interest</div>
+										<div class="col-sm">Principal Payment</div>
+										<div class="col-sm">Remaining Principal</div>
+									</div>
+									<hr>
+									@foreach($payroll->pivot->payment_schedules as $payment_schedule)
+										<div class="row">
+										<div class="col-sm-1">{{ $payment_schedule->term }}</div>
+										<div class="col-sm">{{ $payment_schedule->expected_payment_date }}</div>
+										<div class="col-sm">{{ $payment_schedule->actual_payment_date }}</div>
+										<div class="col-sm">{{ $payment_schedule->total_payment }}</div>
+										<div class="col-sm">{{ $payment_schedule->interest }}</div>
+										<div class="col-sm">{{ $payment_schedule->principal_payment }}</div>
+										<div class="col-sm">{{ $payment_schedule->remaining_principal }}</div>
+									</div>
+									@endforeach
+								</div>
+							</div>
+						@endforeach
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
 	</div>
+	
 @endsection
