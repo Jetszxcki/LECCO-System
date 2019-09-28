4965d1cb60e258cbbe478dfb96422569c9c8e909
Jeff Khirvy Sarmen <khirv_19@yahoo.com>
2019-09-28 20:00:55 +0800

    migration(payment_schedule: create payment schedule migration

---
+++ database/migrations/2019_09_28_181515_create_payment_schedules_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_schedules', function (Blueprint $table) {
+            $table->bigInteger('loan_payroll_id');
+            $table->bigInteger('term');
+            $table->date('expected_payment_date');
+            $table->date('actual_payment_date')->nullable();
+            $table->decimal('total_payment',12,2);
+            $table->decimal('interest',12,2);
+            $table->decimal('principal_payment',12,2);
+            $table->decimal('remaining_principal',12,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_schedules');
+    }
+}
