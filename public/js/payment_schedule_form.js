8cab4a6afa9dbe8d86bd9a1f6bf3749c4feae935
Jeff Khirvy Sarmen <khirv_19@yahoo.com>
2019-09-28 19:59:38 +0800

    feature(loan_create): include hidden field for payment schedule

--- public/js/payment_schedule_form.js
+++ public/js/payment_schedule_form.js
@@ -7,6 +7,15 @@
 payment_schedule_form_lib = function(){
 	function auto_fill(parent, details){
 		// auto fill hidden payment schedule form
+		
+		var hidden_field = document.getElementsByName("payment_schedule")[0];
+		if(!hidden_field){
+			hidden_field = document.createElement("input");
+			hidden_field.setAttribute("type", "hidden");
+			hidden_field.setAttribute("name", "payment_schedule");
+			parent.appendChild(hidden_field);
+		}
+		hidden_field.setAttribute("value", JSON.stringify(details.payment_schedule));
 	}
   return{
 	auto_fill: auto_fill
