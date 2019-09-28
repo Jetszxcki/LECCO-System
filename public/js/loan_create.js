62a91da313a7d63c3204d7e76502bdbd89549dbf
Jetszxcki <jgalbano1@up.edu.ph>
2019-09-28 19:04:39 +0800

    feature(loan_create): improved layout of loans form

--- public/js/loan_create.js
+++ public/js/loan_create.js
@@ -20,10 +20,11 @@ function include_utils(){
 //this is main execution for changes in form
 function update_loan_detail(){
 	var loan_details_div = document.getElementById("loan-details");
+	var payment_schedule_div = document.getElementById("payment-schedule-details");
 	var payment_schedule_hidden_div = document.getElementById("payment-schedule-hidden-form");
 
 	// Just checking if div exists
-	if(!(loan_details_div && payment_schedule_hidden_div)){
+	if(!(loan_details_div && payment_schedule_hidden_div && payment_schedule_div)){
 		alert('Cannot find element to render loan details!');
 		return;
 	}
@@ -34,7 +35,7 @@ function update_loan_detail(){
 	var start_of_payment_field = document.getElementById("start_of_payment");
 	var payrolls_field = document.getElementById("payrolls");
 
-	var loan_details_holder = document.getElementById("loan-details-holder");
+	var parent_loan_details_holder = document.getElementById("parent-loan-details-holder");
 	var loan_form_holder = document.getElementById("loan-form");
 
 	// Just checking if all fields needed exists
@@ -42,15 +43,16 @@ function update_loan_detail(){
 		// loan_details_div.innerHTML = "Cannot find all fields necessary..";
 		loan_form_holder.classList.remove("col-md-8");
 		loan_form_holder.classList.add("col-md-6");
-		loan_details_holder.style.display = "";
+		parent_loan_details_holder.style.display = "";
 	} else {
 		loan_form_holder.classList.remove("col-md-6");
 		loan_form_holder.classList.add("col-md-8");
-		loan_details_holder.style.display = "none";
+		parent_loan_details_holder.style.display = "none";
 		return;
 	}
 	
 	loan_details_div.innerHTML = "";
+	payment_schedule_div.innerHTML = "";
 	
 	//Compile base loan details to object
 	var details = new Object();
@@ -63,7 +65,7 @@ function update_loan_detail(){
 	for(var i = 0; i < details.payrolls.length; i++){
 		var option_field = payrolls_field.querySelector('option[value="'+details.payrolls[i]+'"]');
 		if(option_field){
-			buffer[details.payrolls[i]] = option_field.innerHTML;
+			buffer[details.payrolls[i]] = ' ' + option_field.innerHTML;
 		}
 	}
 	details.payrolls = buffer;
@@ -75,9 +77,8 @@ function update_loan_detail(){
 	details.payment_schedule = loan_utils.calculate_payment_schedule(details);
 
 	//rendering starts here
-
 	loan_details_header(loan_details_div, details);
-	loan_payments_table(loan_details_div, details);
+	loan_payments_table(payment_schedule_div, details);
 	payment_schedule_form_lib.auto_fill(payment_schedule_hidden_div, details);
 }
 
@@ -86,14 +87,14 @@ function loan_details_header(parent, details){
 	var header_div = document.createElement("DIV");
 	// var title_elem = document.createElement("H4");
 	
-	var amount_elem = document.createElement("P");
-	var term_elem = document.createElement("P");
-	var total_interest_elem = document.createElement("P");
-	var total_payable_elem = document.createElement("P");
-	var monthly_payment_elem = document.createElement("P");
-	var ipa_elem = document.createElement("P");
-	var sop_elem = document.createElement("P");
-	var payrolls_elem = document.createElement("P");
+	var amount_elem = document.createElement("DIV");
+	var term_elem = document.createElement("DIV");
+	var total_interest_elem = document.createElement("DIV");
+	var total_payable_elem = document.createElement("DIV");
+	var monthly_payment_elem = document.createElement("DIV");
+	var ipa_elem = document.createElement("DIV");
+	var sop_elem = document.createElement("DIV");
+	var payrolls_elem = document.createElement("DIV");
 	
 	
 	// title_elem.innerHTML = "Loan Details";
@@ -106,9 +107,32 @@ function loan_details_header(parent, details){
 	sop_elem.innerHTML = "Start of Payment: " + details.sop;
 	payrolls_elem.innerHTML = "Payrolls: " + Object.values(details.payrolls);
 	
-	
+	header_div.setAttribute('class', 'row justify-content-center');
 	parent.appendChild(header_div);
 	// header_div.appendChild(title_elem);
+
+	var details_arr = [
+		amount_elem,
+		term_elem,
+		total_interest_elem,
+		total_payable_elem,
+		monthly_payment_elem,
+		ipa_elem,
+		sop_elem,
+		payrolls_elem
+	];
+
+	// simply styles the loan details text
+	details_arr.map((div) => {
+		div.style.letterSpacing = "1px";
+		div.setAttribute('class', 'row col-md-12');
+
+		let text = div.innerHTML;
+		// let detail_label = text.substring(0, text.indexOf(':') + 1);
+		let detail_value = text.substring(text.indexOf(':') + 1, text.length);
+
+		div.innerHTML = text.replace(detail_value, "<label style='font-weight: bold;' class='ml-2'>" + detail_value + "</label>");
+	});
 	
 	header_div.appendChild(amount_elem);
 	header_div.appendChild(term_elem);
@@ -123,9 +147,9 @@ function loan_details_header(parent, details){
 
 function loan_payments_table(parent, details){
 	var loan_payments_div = document.createElement("DIV");
-	var title_elem = document.createElement("H4");
+	// var title_elem = document.createElement("H4");
 	var table_div = document.createElement("DIV");
-	title_elem.innerHTML = "Loan Schedule";
+	// title_elem.innerHTML = "Loan Schedule";
 	
 	var term = details.term;
 	var payrolls = Object.values(details.payrolls);
@@ -137,6 +161,7 @@ function loan_payments_table(parent, details){
 		
 		payroll_header.innerHTML = `${payrolls[j]}`;
 		payroll_div.setAttribute("id", `${payrolls[j]}-schedule`);
+		payroll_div.style.fontSize = "x-small";
 		
 		for(var i = 0; i < term; i++){
 			var row = document.createElement("DIV");
@@ -146,7 +171,7 @@ function loan_payments_table(parent, details){
 			payment_num.setAttribute('class', 'col-sm-1');
 			
 			var expected_payment_date = document.createElement("DIV");
-			expected_payment_date.setAttribute('class', 'col-sm');
+			expected_payment_date.setAttribute('class', 'col-sm-3');
 			
 			var total_payment = document.createElement("DIV");
 			total_payment.setAttribute('class', 'col-sm');
@@ -185,19 +210,15 @@ function loan_payments_table(parent, details){
 		table_div.innerHTML = table_div.innerHTML+ "<hr>";
 	}
 	
-	loan_payments_div.appendChild(title_elem);
+	// loan_payments_div.appendChild(title_elem);
 	loan_payments_div.appendChild(table_div);
 	
 	parent.appendChild(loan_payments_div);
 }
 
-function check_field_values(...fields) {
-
-}
-
 //add event listeners to specific form inputs
 $(document).ready(function() {
-	var loan_details = document.getElementById("loan-details");
+	// var loan_details = document.getElementById("loan-details");
 	
 	var amount_field = document.getElementById("amount");
 	var term_field = document.getElementById("term");
